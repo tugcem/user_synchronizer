39646c4e8d442648b1570f864207632326537236
tugcem <tugcemyalcin@gmail.com>
2018-04-16 10:51:34 +1000

    Inits rspec and endpoint test for the app

--- Gemfile
+++ Gemfile
@@ -3,3 +3,8 @@ source "https://rubygems.org"
 gem 'sinatra', '~> 1.4', '>= 1.4.7'
 gem 'sidekiq'
 gem 'foreman'
+
+group :test, :development do
+  gem 'rspec'
+  gem 'rack-test'
+end
---
+++ spec/app_spec.rb
@@ -0,0 +1,29 @@
+require 'spec_helper'
+require 'rack/test'
+require_relative '../app'
+
+RSpec.describe 'User Synchronizer App' do
+  include Rack::Test::Methods
+
+  def app
+    App.new
+  end
+
+  describe "POST /create_user" do
+    before do
+      data = {
+        name:   'Faker Fake',
+        email:  'faker@fake.com',
+        user_id:'99999999'
+      }
+      post '/create_user', data.to_json
+    end
+    it "should return status 200" do
+      expect(last_response.status).to eq 200
+    end
+
+    it "should include specific body" do
+      expect(last_response.body).to include 'Syncing user in background:'
+    end
+  end
+end
