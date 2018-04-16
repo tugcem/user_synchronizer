f5d3756f5c509df141d6f3921b772c9453d763d5
tugcem <tugcemyalcin@gmail.com>
2018-04-16 11:23:19 +1000

    Test includes worker enqueue.

--- spec/app_spec.rb
+++ spec/app_spec.rb
@@ -25,5 +25,16 @@ RSpec.describe 'User Synchronizer App' do
     it "should include specific body" do
       expect(last_response.body).to include 'Syncing user in background:'
     end
+
+    it "should enqueue job to Users::CreateWorker" do
+      expect do
+        data = {
+          name:   'Faker Fake',
+          email:  'faker@fake.com',
+          user_id:'99999999'
+        }
+        post '/create_user', data.to_json
+      end.to change(Users::CreateWorker.jobs, :size).by(1)
+    end
   end
 end
